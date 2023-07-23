587978619d336cf818db4faec46d00fb24f69271
shivar1007 <shivar100796@gmail.com>
2023-07-23 18:31:51 -0400

    initial commit

---
+++ src/main/java/com/hello/hello/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.hello.hello.controller;
+
+import com.hello.hello.model.Employee;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public Employee firstPage() {
+
+        Employee emp = new Employee();
+        emp.setName("emp1");
+        emp.setDesignation("manager");
+        emp.setEmpId("1");
+        emp.setSalary(3000);
+
+        return emp;
+    }
+
+}
